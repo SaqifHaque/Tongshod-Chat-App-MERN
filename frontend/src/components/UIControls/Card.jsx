5cc7df12e40297a816b7517e4b00bee8974c2323
Saqif <saqifhaque@gmail.com>
2023-02-13 17:31:28 +0600

    Font as prop in card element added

--- frontend/src/components/UIControls/Card.jsx
+++ frontend/src/components/UIControls/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Card = ({children, title}) => {
+const Card = ({children, title, icon}) => {
   return (
     <div className="flex flex-1 justify-center items-center shadow-2xl w-1/2 my-28 mx-auto bg-gray-800 rounded-lg">
-            <div className="w-2/3 flex flex-col items-center justify-center gap-y-8 text-gray-200 font-sans">
-                <span className="text-2xl">{title}</span>
+            <div className="w-2/3 flex flex-col items-center justify-center gap-y-8 text-gray-200 font-sans">           
+                <span className="text-2xl flex flex-row">{icon}{title}</span>
                 {children}
             </div>
         </div>
