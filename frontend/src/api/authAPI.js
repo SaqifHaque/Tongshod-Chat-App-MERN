5f1426472909e7f92c7f297ece1b873ad77d6a14
Saqif <saqifhaque@gmail.com>
2023-02-15 19:00:25 +0600

    auth api configuration in frontend

---
+++ frontend/src/api/authAPI.js
@@ -0,0 +1,29 @@
+import Axios from 'axios';
+import { errorHandler } from '../utils/errorHandler';
+
+const axios = Axios.create({
+    baseURL: `${process.env.REACT_APP_API_URL}/api/auth`,
+    headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+
+
+export const sendOtp = async (data) => {
+    try {
+        const response = await axios.post(`/send-otp`, data);
+        return response.data;
+    } catch (error) {
+       errorHandler(error);
+    }
+}
+
+export const verifyOtp = async (data) => {
+    try {
+        const response = await axios.post(`/send-otp`, data);
+        return response.data;
+    } catch (error) {
+       errorHandler(error);
+    }
+}
