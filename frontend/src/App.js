71376e283b6521d28241009217f28e26ed62fd6d
Saqif <saqifhaque@gmail.com>
2023-02-17 14:49:20 +0600

    axios defaults added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./configurations/router";
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
