8c44d7ce30f8012b375742da198e9322f2d231a0
Saqif <saqifhaque@gmail.com>
2023-02-17 14:48:32 +0600

    working on step avatar page

--- frontend/src/pages/register/steps/StepAvatar.jsx
+++ frontend/src/pages/register/steps/StepAvatar.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import PublicLayout from '../../../components/shared/PublicLayout'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Button from '../../../components/UIControls/Button'
+import Card from '../../../components/UIControls/Card'
+import avatar from '../../../assets/avatar.png';
+import { SET_AVATAR } from '../../../redux/features/activate/activateSlice';
+import { activate } from '../../../api/authAPI';
 
-const StepAvatar = () => {
+const StepAvatar = ({onNext}) => {
+  const dispatch = useDispatch();
+  const { name, avatar } = useSelector((state) => state.activate);
+  const {image, setImage} = useState(avatar);
+
+  const captureImage = (e) => {
+    const file = e.target.files[0];
+    const previewImage = URL.createObjectURL(e.target.files[0])
+    setImage(previewImage);
+    dispatch(SET_AVATAR(previewImage))
+    
+
+    console.log(e);
+  }
+
+  const onSubmit = async () => {
+    try {
+       const { data } = await activate({ name, image });
+    } catch(error) {
+
+    }
+  }
+
   return (
-    <PublicLayout>
-        
-    </PublicLayout>
+    <Card title={`Okay, ${name}`}>
+       <p className="text-center font-thin text-gray-300 font-md font-sans">How's this photo?</p>
+       <div>
+        <img className="w-24 h-24 p-1 rounded-full ring-2 ring-blue-600 dark:ring-gray-500" src={avatar} alt="avatar" loading="lazy"/>
+       </div>
+       <div>
+          <input onChange={(e) => captureImage(e)} id="avatarInput" type="file" className="hidden"/>
+          <label htmlFor="avatarInput" className="text-blue-500 underline text-xs font-md font-sans">Choose a different photo</label>
+      </div>
+      <Button onButtonClick={onNext} title="Next  ->"></Button>
+  </Card>
   )
 }
 
