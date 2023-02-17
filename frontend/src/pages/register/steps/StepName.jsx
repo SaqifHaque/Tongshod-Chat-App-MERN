abc324da65ae1c40478d38e780055e6bdd67c2a4
Saqif <saqifhaque@gmail.com>
2023-02-17 14:48:42 +0600

    working on step name page

--- frontend/src/pages/register/steps/StepName.jsx
+++ frontend/src/pages/register/steps/StepName.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import PublicLayout from '../../../components/shared/PublicLayout'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../../../components/UIControls/Button'
+import Card from '../../../components/UIControls/Card'
+import { SET_NAME } from '../../../redux/features/activate/activateSlice';
 
-const StepName = () => {
+const StepName = ({onNext}) => {
+  const name =  useSelector((state) => state.activate);
+  const dispatch = useDispatch();
+  const [fullName, setFullName] = useState(name);
+
+  const nextStep = () => {
+    if(!name) {
+      return 
+    }
+    dispatch(SET_NAME(fullName));
+    onNext();
+  }
+
   return (
-    <PublicLayout>
-        
-    </PublicLayout>
+    <Card title={"What's your full name?"}>
+        <input onChange={(e) => setFullName(e.target.value)} placeholder="Your name" className="bg-gray-700 rounded-full py-1 w-44 px-3"></input>
+        <Button onButtonClick={nextStep} title="Next  ->"></Button>
+        <p className="text-center font-thin text-gray-300 font-md font-sans">People use real names at tongshod.</p>
+    </Card>
   )
 }
 
