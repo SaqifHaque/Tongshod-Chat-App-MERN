effa3cb602d0e86d76d40b390c3d8a2a98f19dc1
Saqif <saqifhaque@gmail.com>
2023-02-24 22:42:48 +0600

    Working on LoaderCard component

--- frontend/src/components/loader/Loader.jsx
+++ frontend/src/components/loader/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from '../UIControls/Card';
 
 const Loader = () => {
     return ReactDOM.createPortal (
         <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
-            <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+            <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"/>
                 <h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
                 <p className="w-1/3 text-center text-white">This may take a few seconds, please don't close this page.</p>
         </div>,
@@ -23,6 +24,15 @@ export const SpinnerImg = () => {
                 <span className="sr-only">Loading...</span>
             </div>
         </div>
+    )
+}
+
+export const LoaderCard = ({message}) => {
+    return(
+    <Card>
+        <span className='text-blue-500 text-xs font-md font-sans'>{ message }</span>
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"/>
+    </Card>
     )
 }
 
