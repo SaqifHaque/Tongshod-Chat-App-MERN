3e5e7b08a550a3b5fb120a27572bca3df071676a
Saqif <saqifhaque@gmail.com>
2023-02-24 23:06:05 +0600

    loading component integration and testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import { RouterProvider } from "react-router-dom";
 import router from "./configurations/router";
 import axios from 'axios';
 import { useLoadingWithRefresh } from "./hooks/useLoadingWithRefresh";
+import PublicLayout from "./components/shared/PublicLayout";
+import Loader from "./components/loader/Loader";
 
 axios.defaults.withCredentials = true;
 
 function App() {
-  const { loading } =useLoadingWithRefresh();
+  const { loading } = useLoadingWithRefresh();
 
-  return loading ? (
-    'Loading...'
-  ) : (
-    <RouterProvider router={router} />
+  return (
+    <>
+      { loading && <Loader/> }
+      <RouterProvider router={router} />
+    </>
   );
 }
 
