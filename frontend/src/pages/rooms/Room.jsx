96a32b7d6b68bd30b1b481183e48ed16aa9f764c
Saqif <saqifhaque@gmail.com>
2023-05-27 22:53:53 +0600

    room Component updated (Configuring audio stream)

--- frontend/src/pages/rooms/Room.jsx
+++ frontend/src/pages/rooms/Room.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { useWebRTC } from '../../hooks/useWebRTC';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Room = () => {
-    const [clients, setClients] = useWebRTC();
+    const {id: roomId} = useParams();
+    const user = useSelector(state => state.auth.user)
+    const [clients, provideRef] = useWebRTC(roomId, user);
 
   return (
     <div>
@@ -10,8 +14,8 @@ const Room = () => {
         {
             clients.map(client=> {
                 return( 
-                    <div>
-                        <audio controls autoplay></audio>
+                    <div key={client.id}>
+                        <audio ref={(instance) => provideRef(instance, client.id) } controls autoplay></audio>
                         <h4>{client.name}</h4>
                     </div>
                 )
